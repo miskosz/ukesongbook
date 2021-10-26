b297afd959819663b8ff938c7f542facfed42012
Michal Szabados <misko.sz@gmail.com>
2021-10-26 23:04:10 +0200

    Smarter chord sorting

--- check_chord_diagrams.py
+++ check_chord_diagrams.py
@@ -34,13 +34,15 @@ def check_file(song_file_str: str) -> str:
     Checks if chord diagrams match the chords used in the song.
     """
     # Preprocess
-    contents_preprocessed = song_file_str.replace("\\#", "sharp")
+    # Remove lines starting with '%'. Replace '\#' with 'sharp'.
+    tex_code = song_file_str
+    tex_code = "\n".join([s for s in tex_code.split("\n") if not s.startswith("%")])
+    tex_code = tex_code.replace("\\#", "sharp")
 
-
-    # Everything between curly brackets in \ukechord{...} up until "_"
+    # Match everything between curly brackets in \ukechord{...} up until "_".
     diagrams = re.findall(
-        r"\n\\ukechord\{([^\}_]+)[^\}]*\}",
-        contents_preprocessed,
+        r"\\ukechord\{([^\}_]+)[^\}]*\}",
+        tex_code,
         re.DOTALL | re.UNICODE,
     )
     # Remove consecutive duplicates
@@ -48,36 +50,80 @@ def check_file(song_file_str: str) -> str:
         x for i, x in enumerate(diagrams)
         if i == len(diagrams) - 1 or x != diagrams[i + 1]
     ]
-    diagrams_sort = sorted(diagrams)
+    diagrams_sort = sorted(diagrams, key=chord_sort_key)
 
     # Between curly brackets in \ch{...}, take all alphanum characters from
     # the beginning, after an optional star. This is to exclude \rep, \beats
     # or other special instructions inside. 
     used_chords = re.findall(
         r"\\ch\{\*?([\w\./]+)[^\}]*\}",
-        contents_preprocessed,
+        tex_code,
         re.DOTALL | re.UNICODE,
     )
     # Sort and make unique. Remove "N.C."
-    used_chords_uniq_sort = sorted(list(set(used_chords) - set(["N.C."])))
-    used_chords_no_simple = sorted(list(set(used_chords) - set(["N.C.", "Am", "C", "F", "G"])))
+    used_chords_uniq_sort = sorted(
+        list(set(used_chords) - set(["N.C."])), key=chord_sort_key
+    )
+    used_chords_no_simple = sorted(
+        list(set(used_chords) - set(["N.C.", "Am", "C", "F", "G"])),
+        key=chord_sort_key,
+    )
 
     ###
     # Checks
     message = ""
     tab = "    "
 
-    if diagrams != sorted(diagrams):
+    if diagrams != diagrams_sort:
         message += f"{tab}Warning: Chord diagrams should be sorted!\n"
         message += f"{tab}{tab}diagrams: {diagrams}\n"
-        message += f"{tab}{tab}sorted: {diagrams_sort}\n"
+        message += f"{tab}{tab}sorted:   {diagrams_sort}\n"
 
     if diagrams_sort != used_chords_uniq_sort and diagrams_sort != used_chords_no_simple:
         message += f"{tab}Warning: Used chords list does not match the diagrams!\n"
-        message += f"{tab}{tab}diagrams: {diagrams_sort}\n"
+        message += f"{tab}{tab}diagrams:    {diagrams_sort}\n"
         message += f"{tab}{tab}used chords: {used_chords_uniq_sort}\n"
 
     return message
+
+
+def chord_sort_key(ch):
+    """
+    >>> chord_sort_key("Abmaj7")
+    ['A', -1, 0, 'maj7']
+    >>> chord_sort_key('Gm')
+    ['G', 0, 1, '']
+    >>> chord_sort_key('Esharpsus4')
+    ['E', 1, 0, 'sus4']
+    >>> chord_sort_key('G7sus4')
+    ['G', 0, 0, '7sus4']
+    """
+    # base note
+    key = [ch[0]]
+    i = 1
+
+    # flat / natural / sharp
+    if i < len(ch) and ch[i] == "b":
+        key.append(-1)
+        i += 1
+    elif ch[i:].startswith("sharp"):
+        key.append(1)
+        i += 5
+    else:
+        key.append(0)
+
+
+    # major / minor
+    if i < len(ch) and ch[i] == "m" and not ch[i:].startswith("maj"):
+        key.append(1)
+        i += 1
+    else:
+        key.append(0)
+
+    # the rest
+    key.append(ch[i:])
+
+    return key
 
 
 if __name__ == "__main__":
